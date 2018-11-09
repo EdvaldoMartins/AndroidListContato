998fb6f3d414ab5d6b4caaa798470f0420b4bf27
Edvaldo Martins <edvaldonuniomartins@gmail.com>
2018-11-09 17:42:45 +0100

    update09112018

--- app/src/main/java/com/srnunios/contatosdemo/Contacto.kt
+++ app/src/main/java/com/srnunios/contatosdemo/Contacto.kt
@@ -1,6 +1,21 @@
 package com.srnunios.contatosdemo
 
-class Contacto {
+import android.support.annotation.NonNull
+
+
+
+class Contacto : Comparable<Contacto>{
+
+    override operator fun compareTo(c: Contacto): Int {
+        if (this.nome < c.nome) {
+            return 1
+        } else if (this.nome > c.nome) {
+            return -1
+        }
+        return 0
+    }
+
+
     var nome: String = String()
     var numero: String = String()
 
